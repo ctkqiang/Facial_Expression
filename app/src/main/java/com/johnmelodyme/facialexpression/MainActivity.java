0002504eb28364f015f8424e8c1f979ee68cd5cc
John Melody Me, Tan Sin Dee <johnmelodyme@icloud.com>
2020-02-11 23:54:35 +0800

    Camera Setting coded <android.camera.api>;

--- app/src/main/java/com/johnmelodyme/facialexpression/MainActivity.java
+++ app/src/main/java/com/johnmelodyme/facialexpression/MainActivity.java
@@ -4,16 +4,21 @@ package com.johnmelodyme.facialexpression;
  *  encourage me and say that I can ,
  *  SO I do So.
  */
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -55,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         ANALYSE = findViewById(R.id.analyse);
         RESET = findViewById(R.id.reset);
 
+        // TODO ANALYSE_BUTTON:
         ANALYSE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,8 +68,17 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: ANALYSE");
             }
         });
+
+        // TODO RESET_BUTTON:
+        RESET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: RESET");
+            }
+        });
     }
 
+    // TODO : ON_START:
     @SuppressLint("SetTextI18n")
     @Override
     public void onStart(){
@@ -72,40 +87,35 @@ public class MainActivity extends AppCompatActivity {
         PROBABILITY.setText("Probability:  ");
     }
 
-    // MENU ITEMS:
+    // TODO MENU ITEMS:
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int ID = item.getItemId();
-
-        if (ID == R.id.about) {
-            Log.d(TAG, "onOptionsItemSelected: \"Developed by John Melody Melissa\" ");
-            new SweetAlertDialog(MainActivity.this)
-                    .setTitleText("Version 1.0.0")
-                    .setContentText("Developed by John Melody Melissa")
-                    .show();
-            Log.d(TAG, "onOptionsItemSelected: ");
-            return true;
-        }
-
-        if (ID == R.id.upload) {
-            Log.d(TAG, "onOptionsItemSelected: UPLOAD");
-            return true;
-        }
-
-        if (ID == R.id.camera){
-            CHECK_PERMISSION(CAMERA);
-            Log.d(TAG, "onOptionsItemSelected: CAMERA");
-            return false;
+        switch (item.getItemId()){
+            case R.id.about:
+                Log.d(TAG, "onOptionsItemSelected: \"Developed by John Melody Melissa\" ");
+                new SweetAlertDialog(MainActivity.this)
+                        .setTitleText("Version 1.0.0")
+                        .setContentText("Developed by John Melody Melissa")
+                        .show();
+                Log.d(TAG, "onOptionsItemSelected: ");
+                break;
+            case R.id.upload:
+                Log.d(TAG, "onOptionsItemSelected: UPLOAD");
+                break;
+            case R.id.camera:
+                CHECK_PERMISSION(CAMERA);
+                Log.d(TAG, "onOptionsItemSelected: CAMERA");
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    // TODO CHECK_PERMISSION:
     public void CHECK_PERMISSION(int REQUEST_CODE){
         switch (REQUEST_CODE){
             case CAMERA:
@@ -120,20 +130,80 @@ public class MainActivity extends AppCompatActivity {
                     },REQUEST_CODE);
                 }
                 break;
+
             case WRITE_STORAGE:
+                int hasWriteStoragePermission;
+                hasWriteStoragePermission = ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if(hasWriteStoragePermission == PackageManager.PERMISSION_GRANTED){
+                    SELECT_IMAGE();
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    }, REQUEST_CODE);
+                }
                 break;
         }
     }
 
+    // TODO SELECT_IMAGE:
+    private void SELECT_IMAGE() {
+        Photofile = MyHelper.createTempFile(Photofile);
+        Intent intent;
+        intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECT_PHOTO);
+    }
+
+    // TODO LAUNCH_CAMERA:
     private void LAUNCH_CAMERA() {
         Intent MEDIA;
         Uri Photo;
-
         Photofile = MyHelper.createTempFile(Photofile);
         MEDIA = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         Photo = FileProvider.getUriForFile(this, getPackageName()
                 + ".provider", Photofile);
         MEDIA.putExtra(MediaStore.EXTRA_OUTPUT, Photo);
         startActivityForResult(MEDIA, TAKE_PHOTO);
+    }
+
+    // TODO onRequestPermissionsResult:
+    @Override
+    public void onRequestPermissionsResult(int REQUESTCODE, @NonNull String[] PERMISSIONS, @NonNull int[] GRANT_RESULTS){
+        super.onRequestPermissionsResult(REQUESTCODE, PERMISSIONS, GRANT_RESULTS);
+        switch(REQUESTCODE){
+            case CAMERA:
+                if (GRANT_RESULTS.length > 0 && GRANT_RESULTS[0] == PackageManager.PERMISSION_GRANTED){
+                    LAUNCH_CAMERA();
+                } else {
+                    REQUEST_PERMISSION(this, REQUESTCODE, R.string.Camera_denied);
+                }
+                break;
+            case WRITE_STORAGE:
+                break;
+        }
+    }
+
+    // TODO REQUEST_PERMISSION:
+    private void REQUEST_PERMISSION(final MainActivity mainActivity, final int requestcode, int message) {
+        Context context;
+        context = MainActivity.this;
+        AlertDialog.Builder ALERT;
+        ALERT = new AlertDialog.Builder(context);
+        ALERT.setMessage(message);
+        ALERT.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+                Intent intent;
+                intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(Uri.parse("package: " + mainActivity.getPackageName()));
+                mainActivity.startActivityForResult(intent, requestcode);
+            }
+        }).setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        }).setCancelable(false).show();
     }
 }
